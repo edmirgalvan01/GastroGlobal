0c0bc44e727e6952d337ef64e26d9bc129d46a4e
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-10-20 10:14:08 -0500

    se creo la clase Restaurant que conectara a base de datos

---
+++ class/Restaurantes.php
@@ -0,0 +1,76 @@
+<?php
+
+class Restaurant {
+    private $id;
+    private $nombre;
+    private $descripcion;
+    private $telefono_contacto;
+    private $direccion;
+    private $id_propietario;
+    private $id_opiniones;
+
+    public function __construct($nombre, $descripcion, $telefono_contacto, $direccion) {
+        $this -> nombre = $nombre;
+        $this -> descripcion = $descripcion;
+        $this -> telefono_contacto = $telefono_contacto;
+        $this -> direccion = $direccion;
+    }
+
+    public function setId($id) {
+        $this -> $id;
+    }
+
+    public function getId() {
+        return $this -> id;
+    }
+
+    public function setNombre($nombre) {
+        $this -> $nombre;
+    }
+
+    public function getNombre() {
+        return $this -> nombre;
+    }
+    
+    public function setDescripcion($descripcion) {
+        $this -> $descripcion;
+    }
+
+    public function getDescripcion() {
+        return $this -> descripcion;
+    }
+    
+    public function setTelefono_contacto($telefono_contacto) {
+        $this -> $telefono_contacto;
+    }
+
+    public function getTelefono_contacto() {
+        return $this -> telefono_contacto;
+    }
+    
+    public function setDireccion($direccion) {
+        $this -> $direccion;
+    }
+
+    public function getDireccion() {
+        return $this -> direccion;
+    }
+    
+    public function setId_propietario($id_propietario) {
+        $this -> $id_propietario;
+    }
+
+    public function getId_propietario() {
+        return $this -> id_propietario;
+    }
+    
+    public function setId_opiniones($id_opiniones) {
+        $this -> $id_opiniones;
+    }
+
+    public function getId_opiniones() {
+        return $this -> id_opiniones;
+    }
+}
+
+?>
