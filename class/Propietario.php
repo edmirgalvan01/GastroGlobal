06ffee1fe5d965b2f384470116436ca674fece65
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-10-21 10:11:33 -0500

    se creo la clase de Propietario.php

---
+++ class/Propietario.php
@@ -0,0 +1,48 @@
+<?php
+
+class Propietario {
+    private $id;
+    private $nombre;
+    private $email;
+    private $password;
+
+    public function __construct($nombre, $email, $password) {
+        $this -> nombre = $nombre;
+        $this -> email = $email;
+        $this -> password = $password;
+    }
+
+    public function setId($id) {
+        $this -> id = $id;
+    }
+
+    public function getId() {
+        return $this -> id;
+    }
+    
+    public function setNombre($nombre) {
+        $this -> nombre = $nombre;
+    }
+
+    public function getNombre() {
+        return $this -> nombre;
+    }
+    
+    public function setEmail($email) {
+        $this -> email = $email;
+    }
+
+    public function getEmail() {
+        return $this -> email;
+    }
+    
+    public function setPassword($password) {
+        $this -> password = $password;
+    }
+
+    public function getPassword() {
+        return $this -> password;
+    }
+}
+
+?>
