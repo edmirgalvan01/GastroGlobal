e5e94a494d87d778cb599d7638a2ba502d74bdbe
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-10-21 10:12:36 -0500

    Se creo el archivo Plantilla.php que servira como plantilla para las consultas SQL

---
+++ class/Plantilla.php
@@ -0,0 +1,11 @@
+<?php
+
+interface Plantilla {
+    public function crear($objeto);
+    public function modificar($objeto);
+    public function eliminar($id);
+    public function leerTodo();
+    public function leer($id);
+}
+
+?>
