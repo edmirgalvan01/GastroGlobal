a8fa2b311642bd22150af0d533a8e58e0115fa36
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-15 12:39:38 -0600

    Se modifico la funcion para mostrar el modal mediante js

--- js/barra-busqueda.js
+++ js/barra-busqueda.js
@@ -2,5 +2,10 @@ var inputSearch = document.getElementById('input__search');
 var results = document.getElementById('search__results');
 
 inputSearch.addEventListener('click', () => {
-   results.classList.toggle('search__results--activate');
+   if (results.classList.contains('search__results--default')) {
+      results.classList.remove('search__results--default');
+      results.classList.add('search__results--activate');
+   } else {
+      results.classList.add('search__results--default');
+   }
 });
