75be67758518804c0d0e8f1d82a8f41d2de0556e
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-15 12:44:56 -0600

    Se modifico el CSS de la barra de busqueda

--- js/barra-busqueda.js
+++ js/barra-busqueda.js
@@ -1,7 +1,9 @@
+//Referencias a nuestro DOM
 var inputSearch = document.getElementById('input__search');
 var results = document.getElementById('search__results');
 
 inputSearch.addEventListener('click', () => {
+   //si existe la clase search__results--default
    if (results.classList.contains('search__results--default')) {
       results.classList.remove('search__results--default');
       results.classList.add('search__results--activate');
