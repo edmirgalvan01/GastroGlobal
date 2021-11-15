832349a28a519086b9f7c6582ce3b991366cb574
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-15 11:18:57 -0600

    Se modifico la funcion para buscar un restaurant para que se haga instantaneamente

--- js/busqueda_rest.js
+++ js/busqueda_rest.js
@@ -1,14 +1,11 @@
-$('#buscar').keypress(function (evento) {
-   if (evento.keycode == 13) {
-      var valor = $(this).val();
-
-      $.ajax({
-         type: 'post',
-         url: '../admin/script/buscar_restaurant.php',
-         data: { busqueda: valor },
-      }).done(function (pagina) {
-         // pagina solo es una prueba
-         $('restaurant').html(); // variable hacia donde ira
-      });
-   }
+$('#buscar').keyup(function (evento) {
+   var valor = $(this).val();
+   $.ajax({
+      type: 'post',
+      url: '../admin/script/buscar_restaurant.php',
+      data: { busqueda: valor },
+   }).done(function (pagina) {
+      // pagina solo es una prueba
+      $('restaurant').html(); // variable hacia donde ira
+   });
 });
