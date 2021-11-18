b2c69c4395781c4af75ffa3451c11b00b60ee1b8
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-18 11:55:44 -0600

    Se modifico el script de leer_restaurant y se crearon variables para almacenar cada informacion

--- scripts/leer_restaurant.php
+++ scripts/leer_restaurant.php
@@ -6,4 +6,12 @@ require_once('../class/DatosRestaurant.php');
 $r = new DatosRestaurant();
 $restaurant = $r->leer(26); //TODO: Recibir ID adaptable a cada restaurant
 
-echo $restaurant;
+$nombre = $restaurant->getNombre();
+$foto = $restaurant->getFotos();
+$descripcion = $restaurant->getDescripcion();
+$horario = $restaurant->getDiasLaboran() . ' ' . $restaurant->getHorarioEntrada() . ' a ' . $restaurant->getHorarioSalida();
+$direccion = $restaurant->getDireccion();
+$telefono_contacto = $restaurant->getTelefono_contacto();
+$especialidad = $restaurant->getEspecialidad();
+
+echo $horario; //TODO: Investigar retorno de variables
