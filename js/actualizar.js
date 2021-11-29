e8f013b033643523f6faa95b31bd5119c9541651
julio45321 <julio45321@gmail.com>
2021-11-28 20:15:05 -0600

    actualizado

--- js/actualizar.js
+++ js/actualizar.js
@@ -2,9 +2,9 @@ $(document).on('click', '#editar_b', function () {
    var date = new FormData(document.getElementById('form_editar_restaurant')); 
    $.ajax({
       type: 'post',
-      url: 'script/modificar_rest.php',
+      url: 'scripts/modificar_rest.php',
       data: date, 
       contentType: false,
       processData: false,
-   }).done(function (resultado) {}); 
+   }).done(function (resultado){}); 
 });
