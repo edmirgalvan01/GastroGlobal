8dc346599f97611eb09a924853b641cd30106e5f
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-11-29 13:03:16 -0600

    Se le dieron estilos al modal

--- modal/modal_restaurant.php
+++ modal/modal_restaurant.php
@@ -6,8 +6,12 @@
             </div>
             <div class="modal-body">
                 <h1 class="modal__titulo">Toks</h1>
+                <h3>Antojitos mexicanos</h3>
                 <img src="./img/jay-wennington-N_Y88TWmGwA-unsplash.jpg" class="modal__img">
-                <p class="modal__descripcion">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae officia quaerat corrupti velit, animi molestias. Pariatur eum iusto corrupti temporibus odit. Qui unde sequi a sapiente consectetur, odit doloribus voluptatum facere hic vero vel earum omnis ipsum nisi architecto quae. Atque, rerum cumque! Cum optio ipsam maiores, officiis tempore enim.</p>
+                <p class="modal__item">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae officia quaerat corrupti velit, animi molestias. Pariatur eum iusto corrupti temporibus odit. Qui unde sequi a sapiente consectetur, odit doloribus voluptatum facere hic vero vel earum omnis ipsum nisi architecto quae. Atque, rerum cumque! Cum optio ipsam maiores, officiis tempore enim.</p>
+                <p class="modal__item">Colonia 27 de septiembre calle Costa Rica #106</p>
+                <p class="modal__item">Lunes a viernes de 8:00 a 15:00</p>
+                <p class="modal__item">7821123456</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
